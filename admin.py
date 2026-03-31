287643f4af9ac1f9ab1ee0cd9be2c0501e75952a
Ayesha Pathan <ap133385@gmail.com>
2026-03-31 17:42:55 +0530

    Refactor admin.py to include new models and clean up
    Refactor admin.py to include Course, Lesson, Instructor, and Learner models. Update LessonAdmin and remove unused admin classes.

--- admin.py
+++ admin.py
@@ -1,39 +1,34 @@
 from django.contrib import admin
-from .models import Question, Choice, Submission
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 
+# Inline for Choice
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 1
 
 
+# Inline for Question
 class QuestionInline(admin.TabularInline):
     model = Question
     extra = 1
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
-    ]
-    inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
-    search_fields = ['question_text']
-
-
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('choice_text', 'question', 'votes')
-
-
-class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ('question', 'selected_choice', 'submitted_at')
-
-
+# Lesson Admin
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('id',)
+    inlines = [QuestionInline]
 
 
+# Question Admin
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+# Register all models
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice, ChoiceAdmin)
-admin.site.register(Submission, SubmissionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
